39086362cce6cea743e7f67c37823bd50ea37f5b
Rajat Ranjan <111688163+rajatranjan2710@users.noreply.github.com>
2023-09-16 19:49:35 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDb = async()=>{
-    const {connection} = await mongoose.connect("mongodb+srv://rajat2710:rajat@2710@cluster0.gl9zw9j.mongodb.net/todolist?retryWrites=true&w=majority")
+    const {connection} = await mongoose.connect(mongodb+srv://rajat2710:rajat@2710@cluster0.gl9zw9j.mongodb.net/todolist?retryWrites=true&w=majority)
     
         console.log(`Database is connected with ${connection.host}`)
     
