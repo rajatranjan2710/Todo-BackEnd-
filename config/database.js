585bb5508fe008f801134cdacf79c44742568855
Rajat Ranjan <111688163+rajatranjan2710@users.noreply.github.com>
2023-09-16 20:07:00 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,10 +1,25 @@
+// import mongoose from "mongoose";
+
+// export const connectDb = async()=>{
+//     console.log("connecting database");
+//     const {connection} = await mongoose.connect("mongodb+srv://rajat2710:rajat%402710@cluster0.gl9zw9j.mongodb.net/todolist?retryWrites=true&w=majority")
+    
+//         console.log(`Database is connected with ${connection.host}`)
+//     console.log("failed")
+    
+// }
+
 import mongoose from "mongoose";
 
-export const connectDb = async()=>{
-    console.log("connecting database");
-    const {connection} = await mongoose.connect("mongodb+srv://rajat2710:rajat@2710@cluster0.gl9zw9j.mongodb.net/todolist?retryWrites=true&w=majority")
-    
-        console.log(`Database is connected with ${connection.host}`)
-    console.log("failed")
-    
-}
+export const connectDb = async () => {
+  try {
+    console.log("Connecting to the database");
+    const { connection } = await mongoose.connect(
+      "mongodb+srv://rajat2710:rajat%402710@cluster0.gl9zw9j.mongodb.net/todolist?retryWrites=true&w=majority"
+    );
+
+    console.log(`Database is connected with ${connection.host}`);
+  } catch (error) {
+    console.error("Failed to connect to the database:", error);
+  }
+};
